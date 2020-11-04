82a4eb6270a066d6df8480604519fab0bf618c67
cj7changjie <foochangjie@gmail.com>
2020-11-04 22:09:44 +0800

    Update createPageProf.php

--- Prototype/createPageProf.php
+++ Prototype/createPageProf.php
@@ -34,6 +34,7 @@ else{
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/vue/dist/vue.js"></script>
+	<script src="https://unpkg.com/axios/dist/axios.min.js"></script>
   </head>
   <body>
     <?php
